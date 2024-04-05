7be316f92ef64dd0e282c84689a92f3768ac3e7c
Sam Puttman <daputter2@gmail.com>
2024-04-05 12:46:57 -0400

    FEAT - Add nation member poro

---
+++ app/poros/member.rb
@@ -0,0 +1,11 @@
+class Member
+  attr_reader :name, :photo_url, :allies, :enemies, :affiliation
+
+  def initialize(data)
+    @name = data[:name]
+    @photo_url = data[:photoUrl]
+    @allies = data[:allies]
+    @enemies = data[:enemies]
+    @affiliation = data[:affiliation]
+  end
+end
