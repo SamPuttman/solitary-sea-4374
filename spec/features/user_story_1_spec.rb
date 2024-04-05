04bfa6127c70a500a1247d34dbc879183cf8235f
Sam Puttman <daputter2@gmail.com>
2024-04-05 12:44:46 -0400

    REFACTOR - Refactor test

--- spec/features/user_story_1_spec.rb
+++ spec/features/user_story_1_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
 
-RSpec.feature "Search Fire Nation Members", type: :feature do
-  scenario "User searches for Fire Nation members", vcr: true do
+RSpec.describe 'Search', type: :feature do
+  it 'allows users to search for members by nation' do
+    VCR.use_cassette('search_by_nation') do
+      visit root_path
 
-    visit '/'
+      select 'Water Tribe', from: 'nation'
+      click_on 'Search For Members'
 
-    select 'Fire Nation', from: 'nation'
+      expect(current_path).to eq search_path
+      expect(page).to have_content('Total members of the Water Tribe: 20')
+      expect(page).to have_content('Notable Members:')
 
-    click_button 'Search For Members'
+      expect(page).to have_css('li', count: 20)
 
-    #expect(current_path).to eq('/search')
-
-    #expect(page).to have_content("Total Members: 100")
-
-    #expect(page).to have_content("Member Info:")
-
-
-    #expect(page).to have_content('@member.name')
-    #add photo test later
-    #expect(page).to have_content('Allies:')
-    #expect(page).to have_content('Enemies:')
-    #expect(page).to have_content('Affiliations:')
-    #end
+      within first('li') do
+        expect(page).to have_css('h3', text: 'Arnook')
+        expect(page).to have_css("img[src='https://vignette.wikia.nocookie.net/avatar/images/0/0c/Chief.png/revision/latest?cb=20140122221730']")
+        expect(page).to have_content('Allies: Aang')
+        expect(page).to have_content('Enemies: Zhao')
+        expect(page).to have_content('Affiliations: Northern Water Tribe')
+      end
+    end
   end
 end
