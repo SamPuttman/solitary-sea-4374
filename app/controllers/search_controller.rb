c249a001b4ca9776f66b98cbb58c3aee6e275f59
Sam Puttman <daputter2@gmail.com>
2024-04-05 12:49:55 -0400

    FEAT - Add per_page param so it returns more than 20 members.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
     nation = params[:nation]
+    per_page = 25
 
-    response = Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?affiliation=#{nation}")
+    response = Faraday.get("https://last-airbender-api.fly.dev/api/v1/characters?affiliation=#{nation}&perPage=#{per_page}")
     data = JSON.parse(response.body)
 
     @total_members = data.length
